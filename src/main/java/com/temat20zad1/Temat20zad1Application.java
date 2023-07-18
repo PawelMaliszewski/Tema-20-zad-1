1fd12c587689d8e932335d526825147a9cc32bec
Pawel Maliszewski <pablo999455@gmail.com>
2023-07-18 09:50:44 +0200

    creation of a project

---
+++ src/main/java/com/temat20zad1/Temat20zad1Application.java
@@ -0,0 +1,13 @@
+package com.temat20zad1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Temat20zad1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Temat20zad1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/temat20zad1/Temat20zad1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temat20zad1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Temat20zad1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
