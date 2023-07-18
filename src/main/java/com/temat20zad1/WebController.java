f71d62e0b29cf170813c57585b40176ff82e9c7b
Pawel Maliszewski <pablo999455@gmail.com>
2023-07-18 21:01:36 +0200

    solution 2

--- src/main/java/com/temat20zad1/WebController.java
+++ src/main/java/com/temat20zad1/WebController.java
@@ -1,9 +1,6 @@
 package com.temat20zad1;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
@@ -18,34 +15,33 @@ public class WebController {
     @GetMapping("/users")
     @ResponseBody
     public String getUsers() {
-        return userDataBase.getUserList().toString();
+        String result = "";
+        for (int i = 0; i < userDataBase.getUserList().size(); i++) {
+            result += userDataBase.getUserList().get(i).toString() + "</br>";
+        }
+        return result;
     }
 
     @GetMapping("/add")
-    @PostMapping("addUserForm")
-    @ResponseBody
-    public RedirectView addUser(@RequestParam(required = false) String imie,
+    public String addUser(@RequestParam(required = false) String imie,
                                 @RequestParam(required = false) String nazwisko,
                                 @RequestParam(required = false) Integer wiek) {
         if (imie == null) {
-            return new RedirectView("err.html");
+            return "err.html";
         }
         addNewUser(imie, nazwisko, wiek);
-        return new RedirectView("success.html");
+        return "success.html";
     }
 
     @PostMapping("/addUserForm")
-    @ResponseBody
-    RedirectView addUserForm(@RequestParam(required = false) String imie,
+    String addUserForm(@RequestParam(required = false) String imie,
                              @RequestParam(required = false, defaultValue = "nie podano") String nazwisko,
                              @RequestParam(required = false, defaultValue = "0") Integer wiek) {
         if (imie.length() < 1) {
-            System.out.println("redirect:/err.html");
-            return new RedirectView("err.html");
+            return "redirect:/err.html";
         }
         addNewUser(imie, nazwisko, wiek);
-        System.out.println("redirect:/success.html");
-        return new RedirectView("success.html");
+        return "redirect:/success.html";
     }
 
     private void addNewUser(String imie, String nazwisko, Integer wiek) {
